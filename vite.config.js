5df27f232f0b35ddaca02d9e1a540a51859ee552
LudwigCW <131026346+LudwigCW@users.noreply.github.com>
2026-01-18 02:49:12 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' // Wichtig für moderne Bibliotheken wie pdf.js
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  }
 })
